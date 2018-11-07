2ed4047946acc5f088391e1313f5af97eb15fb59
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:24:42 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,33 +1,35 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{ipconnection::IpConnection, motion_detector_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, motion_detector_v2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Motion Detector Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Motion Detector Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let motion_detector_v2_bricklet = MotionDetectorV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let md = MotionDetectorV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for motion detected events.
-    let motion_detected_listener = motion_detector_v2_bricklet.get_motion_detected_receiver();
-    // Spawn thread to handle received events. This thread ends when the motion_detector_v2_bricklet
+    // Create receiver for motion detected events.
+    let motion_detected_receiver = md.get_motion_detected_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `md` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in motion_detected_listener {
+        for motion_detected in motion_detected_receiver {
             println!("Motion Detected");
         }
     });
 
-    //Create listener for detection cycle ended events.
-    let detection_cycle_ended_listener = motion_detector_v2_bricklet.get_detection_cycle_ended_receiver();
-    // Spawn thread to handle received events. This thread ends when the motion_detector_v2_bricklet
+    // Create receiver for detection cycle ended events.
+    let detection_cycle_ended_receiver = md.get_detection_cycle_ended_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `md` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in detection_cycle_ended_listener {
+        for detection_cycle_ended in detection_cycle_ended_receiver {
             println!("Detection Cycle Ended (next detection possible in ~2 seconds)");
         }
     });
--- software/examples/rust/example_indicator.rs
+++ software/examples/rust/example_indicator.rs
@@ -1,20 +1,20 @@
 use std::{error::Error, io};
 
-use tinkerforge::{ipconnection::IpConnection, motion_detector_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, motion_detector_v2_bricklet::*};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Motion Detector Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Motion Detector Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let motion_detector_v2_bricklet = MotionDetectorV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let md = MotionDetectorV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
     // Turn blue backlight LEDs on (maximum brightness)
-    motion_detector_v2_bricklet.set_indicator(255, 255, 255);
+    md.set_indicator(255, 255, 255);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
