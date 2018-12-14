72674d4457042a70a9a1bb904addc9f7c740490c
Erik Fleckstein <erik@tinkerforge.com>
2018-12-14 16:12:53 +0100

    Add go examples.

---
+++ software/examples/go/example_callback.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"tinkerforge/ipconnection"
+	"tinkerforge/motion_detector_v2_bricklet"
+)
+
+const ADDR string = "localhost:4223"
+const UID string = "XYZ" // Change XYZ to the UID of your Motion Detector Bricklet 2.0.
+
+func main() {
+	ipcon := ipconnection.New()
+	defer ipcon.Close()
+	md, _ := motion_detector_v2_bricklet.New(UID, &ipcon) // Create device object.
+
+	ipcon.Connect(ADDR) // Connect to brickd.
+	defer ipcon.Disconnect()
+	// Don't use device before ipcon is connected.
+
+	md.RegisterMotionDetectedCallback(func() {
+		fmt.Println("Motion Detected")
+	})
+
+	md.RegisterDetectionCycleEndedCallback(func() {
+		fmt.Println("Detection Cycle Ended (next detection possible in ~2 seconds)")
+	})
+
+	fmt.Print("Press enter to exit.")
+	fmt.Scanln()
+
+}
