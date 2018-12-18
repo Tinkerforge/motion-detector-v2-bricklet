d795e06e5356506a9c31557f2f96ea0219fed609
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:57:51 +0100

    go: Change import path.

--- software/examples/go/example_indicator.go
+++ software/examples/go/example_indicator.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/motion_detector_v2_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/motion_detector_v2_bricklet"
 )
 
 const ADDR string = "localhost:4223"
