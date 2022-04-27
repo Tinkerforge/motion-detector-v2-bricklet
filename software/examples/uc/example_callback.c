f60a3592c4a9242e1d7bed9405243d6e815b9fda
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 15:26:01 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_callback.c
+++ software/examples/uc/example_callback.c
@@ -1,17 +1,13 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_motion_detector_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_motion_detector_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Motion Detector Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 // Callback function for motion detected callback
 static void motion_detected_handler(TF_MotionDetectorV2 *device, void *user_data) {
@@ -31,7 +27,7 @@ static TF_MotionDetectorV2 md;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_motion_detector_v2_create(&md, UID, hal), "create device object");
+	check(tf_motion_detector_v2_create(&md, NULL, hal), "create device object");
 
 	// Register motion detected callback to function motion_detected_handler
 	tf_motion_detector_v2_register_motion_detected_callback(&md,
--- software/examples/uc/example_indicator.c
+++ software/examples/uc/example_indicator.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_motion_detector_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_motion_detector_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Motion Detector Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_MotionDetectorV2 md;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_motion_detector_v2_create(&md, UID, hal), "create device object");
+	check(tf_motion_detector_v2_create(&md, NULL, hal), "create device object");
 
 	// Turn blue backlight LEDs on (maximum brightness)
 	check(tf_motion_detector_v2_set_indicator(&md, 255, 255, 255), "call set_indicator");
